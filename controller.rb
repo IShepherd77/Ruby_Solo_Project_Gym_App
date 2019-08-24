c125afb2b0e3b27acdac41f10a55899a575c19d7
Ian Shepherd <ianshepherd77@gmail.com>
2019-08-24 12:19:43 +0100

    added edit and date format

--- controller.rb
+++ controller.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require('pry')
 require_relative('models/member')
 also_reload('./models/*')
 
@@ -12,4 +13,12 @@ post '/members/:id/delete' do
   member = Member.find(params['id'])
   member.delete
   redirect to '/members'
+end
+
+get '/members/:id/edit' do # edit
+  @member = Member.find( params[:id] )
+  #converting database string date to Ruby date object
+  @member.dob=(Date.parse(@member.dob))
+  binding.pry
+  erb( :edit_member )
 end
